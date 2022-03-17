7935c299d5399978f14424f86d21d83ae4a7c0da
milkCXB <1436296421@qq.com>
2022-03-17 17:05:15 +0800

    配置

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode, Param, Post, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('animals')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  // constructor(private readonly appService: AppService) {}
+  @Get('/dogs')
+  create(): string {
+  
+    return 'This action adds a new cat';
   }
+
+  // @Get('cats')
+  // findAll() {
+  //   console.log('444阿1松大');
+  //   return '12344'
+    
+  // }
+
+  @Get(':id')
+  findOne(@Param('id') id): string {
+    // console.log(params.id);
+    return `This action returns a #${id} cat`;
+  }
+
+  // getHello(): string {
+  //   return 'HELLO';
+  // }
 }
